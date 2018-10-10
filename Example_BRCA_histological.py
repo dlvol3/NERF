98790ebb43855285b640a1165fc0fa6fa1d25f14
dlvol3 <dlvol3@me.com>
2018-10-10 15:37:04 +0200

    Re-import Laptinib

--- Example_BRCA_histological.py
+++ Example_BRCA_histological.py
@@ -38,7 +38,7 @@ y = B20000.iloc[:, 0].values  # Labels of training
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.01, random_state=123)
 
-rf1 = RandomForestClassifier(n_estimators=300, criterion='gini', max_features="sqrt",
+rf1 = RandomForestClassifier(n_estimators=30, criterion='gini', max_features="sqrt",
                              oob_score=True, n_jobs=13, max_depth=14,
                              verbose=0)
 
@@ -50,3 +50,264 @@ t1 = localnerf(nt_his, 1)
 t2 = localnerf(nt_his, 2)
 t1.to_csv('testoutput_his1.txt', sep='\t')
 t2.to_csv('testoutput_his0.txt', sep='\t')
+
+
+# Lap example
+#%%
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn import cross_validation
+from sklearn.preprocessing import LabelEncoder
+from sklearn.datasets import make_classification
+
+import platform
+#%%
+if platform.system() == 'Windows':
+    # Windows in the lab
+    gdscic = pd.read_csv('P:/VM/Drug/data/output/GDSCIC50.csv')
+    ccleic = pd.read_csv('P:/VM/Drug/data/output/CCLEIC50.csv')
+if platform.system() == 'Darwin':
+    # My mac
+    gdscic = pd.read_csv('/Users/yue/Pyc/Drug2018Sep/data/GDSCIC50.csv')
+    ccleic = pd.read_csv('/Users/yue/Pyc/Drug2018Sep/data/CCLEIC50.csv')
+
+# Extract the Lat. Drug data from both of the datasets
+gdscic.head(5)
+lapagdsc = gdscic.loc[(gdscic.drug == 'Lapatinib')]
+lapaccle = ccleic.loc[(ccleic.drug == 'Lapatinib')]
+
+# Create list for subset
+ciLapa = list(range(3, len(lapaccle.columns), 1))
+ciLapa.insert(0, 1)
+
+# subset two sets
+lapaC = lapaccle.iloc[:, ciLapa]
+lapaG = lapagdsc.iloc[:, ciLapa]
+lapaC.head(1)
+#%%
+# -------------------------###
+# Get familiar with python DS
+# ref: https://www.kaggle.com/willkoehrsen/start-here-a-gentle-introduction
+
+# Label the class
+le = LabelEncoder()
+le_count = 0
+
+# iterate through columns
+for col in lapaC:
+    if lapaC.loc[:, col].dtype == 'object':
+        # if less than 2 classes(which is better to use one-hot coding if not)
+        if len(list(lapaC.loc[:, col].unique())) <= 2:
+            # 'train' the label encoder with the training data
+            le.fit(lapaC.loc[:, col])
+            # Transform both training and testing
+            lapaC.loc[:, col] = le.transform(lapaC.loc[:, col])
+            lapaG.loc[:, col] = le.transform(lapaG.loc[:, col])
+
+            # Keep track of how many columns were labeled
+            le_count += 1
+
+print('%d columns were label encoded.' % le_count)
+
+#%%
+# Exploratory Data Analysis(EDA)
+
+# Distribution of the target classes(columns)
+lapaC['SENRES'].value_counts()
+lapaC['SENRES'].head(4)
+
+lapaC['SENRES'].plot.hist()
+plt.show()
+
+#%%
+# Examine Missing values
+def missing_value_table(df):
+    # Total missing values
+    mis_val = df.isnull().sum()
+
+    # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum()/len(df)
+
+    # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+
+    # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(
+        columns={0: 'Missing values', 1: '% of Total Values'}
+    )
+
+    # Sort the table by percentage of the missing values
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+
+    # Print the summary
+    print("Your selected data frame has " + str(df.shape[1]) + " columns.\n"
+          "There are " + str(mis_val_table_ren_columns.shape[0]) +
+          " columns that have missing values.")
+
+    # Return the result
+    return mis_val_table_ren_columns
+
+
+# Check the missing value in the dataset
+
+Missing_values = missing_value_table(lapaC)
+Missing_values.head(10)
+#%%
+# Column Types
+# Number of each type of column
+lapaC.dtypes.value_counts()
+
+# Check the number of the unique classes in each object column
+lapaC.select_dtypes('object').apply(pd.Series.nunique, axis=0)
+
+#%%
+# Correlations
+correlations = lapaC.iloc[:, 0:200].corr()['SENRES'].sort_values(na_position='first')
+
+# Display correlations
+print('Most Positive Correlations:\n', correlations.tail(15))
+print('\nMost Negative Correlations:\n', correlations.head(15))
+# Create Cross-validation and training/testing
+
+
+#%%
+# Random forest 1st
+
+# Define the RF
+random_forest = RandomForestClassifier(n_estimators=300, random_state=123, max_features="sqrt",
+                                       criterion="gini", oob_score=True, n_jobs=10, max_depth=12,
+                                       verbose=0)
+#%%
+# Drop SENRES
+
+train_labels = lapaC.loc[:, "SENRES"]
+cell_lines_lapaC = lapaC.loc[:, "ccle.name"]
+lapaC = lapaC.drop(['ccle.name'], axis=1)
+
+if 'SENRES' in lapaC.columns:
+    train = lapaC.drop(['SENRES'], axis=1)
+else:
+    train = lapaC.copy()
+train.iloc[0:3,0:3]
+features = list(train.columns)
+# train["SENRES"] = train_labels
+
+
+
+#%%
+
+# RF 1st train 5 trees
+
+random_forest.fit(train, train_labels)
+
+# Extract feature importances
+feature_importance_values = random_forest.feature_importances_
+feature_importances = pd.DataFrame({'feature': features, 'importance': feature_importance_values})
+feature_importances
+train.shape
+# Make predictions on the test data
+test_labels = lapaG.loc[:, "SENRES"]
+cell_lines_lapaG = lapaG.loc[:, "gdsc.name"]
+
+if 'SENRES' in lapaG.columns:
+    test = lapaG.drop(['SENRES'], axis=1)
+else:
+    test = lapaG.copy()
+
+test = test.drop(['gdsc.name'], axis=1)
+predictions = random_forest.predict(test)
+predictions
+
+confusion_matrix(test_labels, predictions)
+#%%
+random_forest.oob_score_
+#
+test_pred = random_forest.predict(test)
+random_forest.decision_path(test)
+len(list(test_pred))
+print(confusion_matrix(test_labels, test_pred))
+random_forest.get_params(deep=True)
+visualize_classifier(random_forest, train, test)
+
+
+
+#%%
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+import graphviz
+tree1 = random_forest.estimators_[2]
+from sklearn import tree
+dotdata = export_graphviz(tree1, out_file=None,
+                feature_names=train.columns,
+                rounded=True, proportion=False,
+                precision=2,filled=True)
+
+graph5 = graphviz.Source(dotdata)
+graph5.render("test")
+from subprocess import call
+import os
+dir_path = os.getcwd()
+
+tree1.tree_.impurity
+
+print(dotdata)
+#%%
+# Check cross-validation result
+# from sklearn.model_selection import cross_val_score
+
+# print(np.mean(cross_val_score(random_forest, train, train_labels, cv=10)))
+# print(cross_val_score(random_forest, train, train_labels, cv=10))
+
+#%%
+# Module for the network approaches
+# Extract the information in the decision tree
+
+n_nodes = random_forest.estimators_[2].tree_.node_count
+children_left = random_forest.estimators_[2].tree_.children_left
+children_right = random_forest.estimators_[2].tree_.children_right
+feature = random_forest.estimators_[2].tree_.feature
+threshold = random_forest.estimators_[2].tree_.threshold
+testy = train.iloc[[2, 4]]
+decision_p = random_forest.decision_path(testy)
+leave_p = random_forest.apply(test)
+decision_p[0].indices
+testy.shape
+print(decision_p)
+#%%
+print(tree1.tree_.children_left)
+
+
+#%%
+for rf_tree in random_forest.estimator:
+    n_nodes = random_forest.estimators_[1].tree_.node_count
+    children_left = random_forest.estimators_[1].tree_.children_left
+#%%
+# Tree pre-order traversal
+for i in range(tree1.tree_.node_count):
+    print("%s, %s, %s"%(i, tree1.tree_.children_left[i], tree1.tree_.children_right[i]))
+#%%
+print(decision_p[1])
+
+
+#%%
+# random_forest.predict(testy)[1]
+
+TIE = flatforest(random_forest, testy)
+nt_lap = nerftab(TIE)
+t1 = localnerf(nt_lap, 1)
+t0 = localnerf(nt_lap, 0)
+t1.to_csv('testoutput_lap1.txt', sep='\t')
+t0.to_csv('testoutput_lap0.txt', sep='\t')
+
+#%%
+
+featurelist = train.columns.values.tolist()
+
+featurelist[26861]
