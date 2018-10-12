187d930da6dc6ad135da3c18cb5ee80680903b36
dlvol3 <dlvol3@me.com>
2018-10-12 14:44:16 +0200

    three cell lines for poster and retreat

--- Example_BRCA_histological.py
+++ Example_BRCA_histological.py
@@ -17,6 +17,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.datasets import make_classification
 import platform
 import time
+import mygene
 
 #
 
@@ -234,7 +235,7 @@ random_forest.decision_path(test)
 len(list(test_pred))
 print(confusion_matrix(test_labels, test_pred))
 random_forest.get_params(deep=True)
-visualize_classifier(random_forest, train, test)
+
 
 
 
@@ -280,21 +281,6 @@ leave_p = random_forest.apply(test)
 decision_p[0].indices
 testy.shape
 print(decision_p)
-#%%
-print(tree1.tree_.children_left)
-
-
-#%%
-for rf_tree in random_forest.estimator:
-    n_nodes = random_forest.estimators_[1].tree_.node_count
-    children_left = random_forest.estimators_[1].tree_.children_left
-#%%
-# Tree pre-order traversal
-for i in range(tree1.tree_.node_count):
-    print("%s, %s, %s"%(i, tree1.tree_.children_left[i], tree1.tree_.children_right[i]))
-#%%
-print(decision_p[1])
-
 
 #%%
 # random_forest.predict(testy)[1]
@@ -309,5 +295,114 @@ t0.to_csv('testoutput_lap0.txt', sep='\t')
 #%%
 
 featurelist = train.columns.values.tolist()
+# Mygene convertion
+mg = mygene.MyGeneInfo()
+mg.metadata('available_fields')
+con = mg.querymany(featurelist, scopes='ensembl.gene', fields='symbol', species="human", as_dataframe=True)
+featurelist_g = con.iloc[:, 3].reset_index()
+feag = featurelist_g.iloc[:, 1]
+feag.pop(7220)
+feag.pop(38223)
+feag.pop(47083)
+feag = list(feag)
+con.shape
+featurelist[0]   # 100+ in original FI, top 5 in NERF
+featurelist.index('ENSG00000236107')
+featurelist.index('ENSG00000229425')
+featurelist.index('ENSG00000233024')
 
-featurelist[26861]
+index = list(range(len(featurelist)))
+
+sl = random_forest.feature_importances_
+fl = pd.DataFrame({
+    'feature_name': featurelist,
+    'score': sl,
+    'index': index
+})
+
+
+fls = fl.sort_values('score', ascending=False)
+
+fls.loc[fls['feature_name'] == 'ENSG00000189077', ]
+#%%
+
+
+# Sort by value, Dict
+def sort_by_value(d):
+    items = d.items()
+    backitems = [[v[1], v[0]] for v in items]
+    backitems.sort(reverse=True)
+    return [backitems[i][1] for i in range(0, len(backitems))]
+#%%
+# NewtworkX
+import math
+import networkx as nx
+
+t0RT = t0.replace(index, feag)
+
+G = nx.from_pandas_edgelist(t0RT, "feature_i", "feature_j", "EI")
+
+DC = nx.degree_centrality(G)
+RTDC = sort_by_value(DC)
+RTDC1 = RTDC[: int(2*math.sqrt(len(RTDC)))]
+
+t0RT = t0RT.sort_values('EI', ascending=False)
+t01 = t0RT[: int(10*math.sqrt(t0RT.shape[0]))]
+
+
+t02 = t01[t01['feature_i'].isin(RTDC1) & t01['feature_j'].isin(RTDC1)]
+t02.to_csv('testoutput_uni.txt', sep='\t')
+
+G = nx.from_pandas_edgelist(t02, "feature_i", "feature_j", "EI")
+nx.draw(G)
+plt.show()
+
+#%%
+# Other cancer
+# A549 lung 16, BT549 Breast 31
+testy2 = train.iloc[[16, 31]]
+TIE2 = flatforest(random_forest, testy)
+nt_lap2 = nerftab(TIE)
+tlung = localnerf(nt_lap2, 0)
+tbreast = localnerf(nt_lap2, 1)
+tlung = tlung.replace(index, feag)
+
+tbreast = tbreast.replace(index, feag)
+tlung.to_csv('testoutput_A549.txt', sep='\t')
+tbreast.to_csv('testoutput_BT549.txt', sep='\t')
+
+featurelist[31107]
+
+tlung = tlung.replace(index, feag)
+
+tbreast = tbreast.replace(index, feag)
+# replace with gene name and selection of 'powerful nodes' lung
+Glung = nx.from_pandas_edgelist(tlung, "feature_i", "feature_j", "EI")
+
+DClung = nx.degree_centrality(Glung)
+RTDClung = sort_by_value(DClung)
+RTDC1lung = RTDClung[: int(2*math.sqrt(len(RTDClung)))]
+
+tlungsort = tlung.sort_values('EI', ascending=False)
+tlungS = tlungsort[: int(10*math.sqrt(t0RT.shape[0]))]
+
+
+tlungfinal = tlungS[tlungS['feature_i'].isin(RTDC1lung) & tlungS['feature_j'].isin(RTDC1lung)]
+tlungfinal.to_csv('testoutput_RTDC1lung.txt', sep='\t')
+
+# replace with gene name and selection of 'powerful nodes' breast
+Gbreast = nx.from_pandas_edgelist(tbreast, "feature_i", "feature_j", "EI")
+
+DCbreast = nx.degree_centrality(Gbreast)
+RTDCbreast = sort_by_value(DCbreast)
+RTDC1breast = RTDCbreast[: int(2*math.sqrt(len(RTDCbreast)))]
+
+tbreastsort = tbreast.sort_values('EI', ascending=False)
+tbreastS = tbreastsort[: int(10*math.sqrt(t0RT.shape[0]))]
+
+
+tbreastfinal = tbreastS[tbreastS['feature_i'].isin(RTDC1breast) & tbreastS['feature_j'].isin(RTDC1breast)]
+tbreastfinal.to_csv('testoutput_RTDC1breast.txt', sep='\t')
+
+#%%
+# Similarity between the two predictions
